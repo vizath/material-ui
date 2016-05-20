cc6d8c8cfa0b3729f383678430ef3092b426d860
Maxime Fafard <maxfafard@gmail.com>
2016-05-20 14:31:03 -0400

    v0.15.0

---
+++ app-bar-new.js
@@ -0,0 +1,253 @@
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _simpleAssign = require('simple-assign');
+
+var _simpleAssign2 = _interopRequireDefault(_simpleAssign);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _Paper = require('./Paper');
+
+var _Paper2 = _interopRequireDefault(_Paper);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+var AppBar = _react2.default.createClass({
+  displayName: 'AppBar',
+
+
+  propTypes: {
+    actionIcons: _react2.default.PropTypes.array,
+    children: _react2.default.PropTypes.node,
+    className: _react2.default.PropTypes.string,
+    filterIcon: _react2.default.PropTypes.element,
+    iconStyleRight: _react2.default.PropTypes.object,
+    moreIcon: _react2.default.PropTypes.element,
+    navIcon: _react2.default.PropTypes.element,
+    onLeftIconButtonTouchTap: _react2.default.PropTypes.func,
+    onRightIconButtonTouchTap: _react2.default.PropTypes.func,
+    style: _react2.default.PropTypes.object,
+    title: _react2.default.PropTypes.node,
+    titleStyle: _react2.default.PropTypes.object,
+    zDepth: _react2.default.PropTypes.number
+  },
+
+  contextTypes: {
+    muiTheme: _react2.default.PropTypes.object.isRequired
+  },
+
+  getDefaultProps: function getDefaultProps() {
+    return {
+      title: '',
+      zDepth: 1
+    };
+  },
+  getStyles: function getStyles() {
+    var spacing = this.context.muiTheme.rawTheme.spacing;
+    var themeVariables = this.context.muiTheme.appBar;
+    var iconButtonSize = this.context.muiTheme.button.iconButtonSize;
+    var flatButtonSize = 36;
+    var styles = {
+      root: {
+        zIndex: 5,
+        width: '100%',
+        display: 'flex',
+        minHeight: themeVariables.height,
+        backgroundColor: themeVariables.color,
+        paddingLeft: spacing.desktopGutter,
+        paddingRight: spacing.desktopGutter
+      },
+      title: {
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        margin: 0,
+        paddingTop: 0,
+        letterSpacing: 0,
+        fontSize: 24,
+        fontWeight: this.context.muiTheme.appBar.titleFontWeight,
+        color: themeVariables.textColor,
+        lineHeight: themeVariables.height + 'px'
+      },
+
+      // http://jsfiddle.net/iamanubhavsaini/zWtBu/
+      mainElement: {
+        flex: '0 1 auto',
+        minWidth: 0
+      },
+      filterIcon: {
+        marginTop: (themeVariables.height - iconButtonSize) / 2,
+        flex: '1 0 auto'
+      },
+      rightContainer: {
+        marginRight: -16,
+        marginLeft: 0,
+        flex: '0 0 auto'
+      },
+
+      iconButton: {
+        style: {
+          marginTop: (themeVariables.height - iconButtonSize) / 2,
+          marginLeft: -16
+        },
+        iconStyle: {
+          fill: themeVariables.textColor,
+          color: themeVariables.textColor
+        }
+      },
+      flatButton: {
+        color: themeVariables.textColor,
+        backgroundColor: 'transparent',
+        marginTop: (iconButtonSize - flatButtonSize) / 2 + 2
+      }
+    };
+
+    return styles;
+  },
+  _onLeftIconButtonTouchTap: function _onLeftIconButtonTouchTap(event) {
+    if (this.props.onLeftIconButtonTouchTap) {
+      this.props.onLeftIconButtonTouchTap(event);
+    }
+  },
+  _onRightIconButtonTouchTap: function _onRightIconButtonTouchTap(event) {
+    if (this.props.onRightIconButtonTouchTap) {
+      this.props.onRightIconButtonTouchTap(event);
+    }
+  },
+  render: function render() {
+    var _props = this.props;
+    var title = _props.title;
+    var titleStyle = _props.titleStyle;
+    var className = _props.className;
+    var style = _props.style;
+    var zDepth = _props.zDepth;
+    var children = _props.children;
+    var navIcon = _props.navIcon;
+    var filterIcon = _props.filterIcon;
+    var actionIcons = _props.actionIcons;
+    var moreIcon = _props.moreIcon;
+    var iconStyleRight = _props.iconStyleRight;
+
+    var other = _objectWithoutProperties(_props, ['title', 'titleStyle', 'className', 'style', 'zDepth', 'children', 'navIcon', 'filterIcon', 'actionIcons', 'moreIcon', 'iconStyleRight']);
+
+    var titleElement = void 0;
+    var navIconElement = void 0;
+    var filterIconElement = void 0;
+    var rightIcons = [];
+    var rightIconElements = void 0;
+
+    var prepareStyles = this.context.muiTheme.prepareStyles;
+
+    var styles = this.getStyles();
+    var iconRightContainerStyle = prepareStyles(styles.iconButton.style, styles.rightContainer, iconStyleRight);
+
+    if (navIcon) {
+      if (navIcon.type.muiName === 'IconButton') {
+        navIcon = _react2.default.cloneElement(navIcon, {
+          iconStyle: (0, _simpleAssign2.default)({}, styles.iconButton.iconStyle, navIcon.props.iconStyle)
+        });
+      }
+
+      navIconElement = _react2.default.createElement(
+        'div',
+        { style: styles.iconButton.style },
+        navIcon
+      );
+    }
+
+    // Title Wrapper
+    if (title) {
+      // If the title is a string, wrap in an h1 tag.
+      // If not, just use it as a node.
+      if (typeof title === 'string' || title instanceof String) {
+        title = _react2.default.createElement(
+          'h1',
+          { style: prepareStyles(styles.title, titleStyle) },
+          title
+        );
+      }
+    }
+
+    // Filter Icon
+    if (filterIcon) {
+      if (filterIcon.type.muiName === 'IconButton' || filterIcon.type.muiName === 'IconMenu') {
+        filterIcon = _react2.default.cloneElement(filterIcon, {
+          iconStyle: (0, _simpleAssign2.default)({}, styles.iconButton.iconStyle, filterIcon.props.iconStyle)
+        });
+      }
+    }
+    filterIconElement = _react2.default.createElement(
+      'div',
+      { style: prepareStyles(styles.filterIcon) },
+      filterIcon
+    );
+
+    // Builded title (text and filter icon)
+    titleElement = _react2.default.createElement(
+      'div',
+      { style: prepareStyles(styles.mainElement) },
+      title
+    );
+
+    // Action Icons
+    if (actionIcons) {
+      actionIcons = actionIcons.map(function (icon) {
+        if (icon.type.muiName === 'IconButton') {
+          icon = _react2.default.cloneElement(icon, {
+            iconStyle: (0, _simpleAssign2.default)({}, styles.iconButton.iconStyle, icon.props.iconStyle)
+          });
+        } else if (icon.type.muiName === 'FlatButton') {
+          icon = _react2.default.cloneElement(icon, {
+            style: (0, _simpleAssign2.default)({}, styles.flatButton, icon.props.style)
+          });
+        }
+        return icon;
+      }.bind(this));
+
+      rightIcons = rightIcons.concat(actionIcons);
+    }
+
+    // More Icon
+    if (moreIcon) {
+      if (moreIcon.type.muiName === 'IconButton' || moreIcon.type.muiName === 'IconMenu') {
+        moreIcon = _react2.default.cloneElement(moreIcon, {
+          iconStyle: (0, _simpleAssign2.default)({}, styles.iconButton.iconStyle, moreIcon.props.iconStyle)
+        });
+      }
+
+      rightIcons.push(moreIcon);
+    }
+
+    // Right-most Icon container
+    if (rightIcons.length > 0) {
+      rightIconElements = _react2.default.createElement(
+        'div',
+        { style: iconRightContainerStyle },
+        rightIcons
+      );
+    }
+
+    return _react2.default.createElement(
+      _Paper2.default,
+      _extends({}, other, {
+        rounded: false,
+        className: className,
+        style: (0, _simpleAssign2.default)({}, styles.root, style),
+        zDepth: zDepth
+      }),
+      navIconElement,
+      titleElement,
+      filterIconElement,
+      rightIconElements,
+      children
+    );
+  }
+});
+
+module.exports = AppBar;
---
+++ table-list.js
@@ -0,0 +1,288 @@
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _simpleAssign = require('simple-assign');
+
+var _simpleAssign2 = _interopRequireDefault(_simpleAssign);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _transitions = require('./styles/transitions');
+
+var _transitions2 = _interopRequireDefault(_transitions);
+
+var _Divider = require('./Divider');
+
+var _Divider2 = _interopRequireDefault(_Divider);
+
+var _EnhancedButton = require('./internal/EnhancedButton');
+
+var _EnhancedButton2 = _interopRequireDefault(_EnhancedButton);
+
+var _colorManipulator = require('./utils/colorManipulator');
+
+var _colors = require('./styles/colors');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
+var TableItem = _react2.default.createClass({
+  displayName: 'TableItem',
+
+  propTypes: {
+    children: _react2.default.PropTypes.node,
+    innerDivStyle: _react2.default.PropTypes.object,
+    onKeyboardFocus: _react2.default.PropTypes.func,
+    onMouseEnter: _react2.default.PropTypes.func,
+    onMouseLeave: _react2.default.PropTypes.func,
+    onNestedListToggle: _react2.default.PropTypes.func,
+    onTouchStart: _react2.default.PropTypes.func,
+    onTouchTap: _react2.default.PropTypes.func,
+    style: _react2.default.PropTypes.object
+  },
+
+  contextTypes: {
+    muiTheme: _react2.default.PropTypes.object.isRequired
+  },
+
+  getDefaultProps: function getDefaultProps() {
+    return {
+      onKeyboardFocus: function onKeyboardFocus() {},
+      onMouseEnter: function onMouseEnter() {},
+      onMouseLeave: function onMouseLeave() {},
+      onTouchStart: function onTouchStart() {}
+    };
+  },
+  getInitialState: function getInitialState() {
+    return {
+      hovered: false,
+      isKeyboardFocused: false,
+      touch: false
+    };
+  },
+  _handleKeyboardFocus: function _handleKeyboardFocus(e, isKeyboardFocused) {
+    this.setState({ isKeyboardFocused: isKeyboardFocused });
+    this.props.onKeyboardFocus(e, isKeyboardFocused);
+  },
+  _handleMouseEnter: function _handleMouseEnter(e) {
+    if (!this.state.touch) this.setState({ hovered: true });
+    this.props.onMouseEnter(e);
+  },
+  _handleMouseLeave: function _handleMouseLeave(e) {
+    this.setState({ hovered: false });
+    this.props.onMouseLeave(e);
+  },
+  _handleNestedListToggle: function _handleNestedListToggle(e) {
+    e.stopPropagation();
+    this.setState({ open: !this.state.open });
+    this.props.onNestedListToggle(this);
+  },
+  _handleTouchStart: function _handleTouchStart(e) {
+    this.setState({ touch: true });
+    this.props.onTouchStart(e);
+  },
+  render: function render() {
+    var _props = this.props;
+    var children = _props.children;
+    var onTouchTap = _props.onTouchTap;
+    var style = _props.style;
+    var innerDivStyle = _props.innerDivStyle;
+
+    var other = _objectWithoutProperties(_props, ['children', 'onTouchTap', 'style', 'innerDivStyle']);
+
+    var prepareStyles = this.context.muiTheme.prepareStyles;
+
+
+    var textColor = this.context.muiTheme.rawTheme.palette.textColor;
+    var hoverColor = (0, _colorManipulator.fade)(textColor, 0.1);
+    var styles = {
+      root: {
+        backgroundColor: this.state.isKeyboardFocused || this.state.hovered ? hoverColor : null,
+        color: textColor,
+        display: 'block',
+        fontSize: 16,
+        lineHeight: '16px',
+        position: 'relative',
+        transition: _transitions2.default.easeOut()
+      },
+
+      //This inner div is needed so that ripples will span the entire container
+      innerDiv: {
+        padding: 16,
+        position: 'relative'
+      }
+    };
+
+    return _react2.default.createElement(
+      _EnhancedButton2.default,
+      _extends({}, other, {
+        linkButton: true,
+        containerElement: 'div',
+        onKeyboardFocus: this._handleKeyboardFocus,
+        onMouseLeave: this._handleMouseLeave,
+        onMouseEnter: this._handleMouseEnter,
+        onTouchStart: this._handleTouchStart,
+        onTouchTap: onTouchTap,
+        ref: 'enhancedButton',
+        style: (0, _simpleAssign2.default)({}, styles.root, style)
+      }),
+      _react2.default.createElement(
+        'div',
+        { style: prepareStyles(styles.innerDiv, innerDivStyle) },
+        children
+      )
+    );
+  }
+});
+
+var TableCell = _react2.default.createClass({
+  displayName: 'TableCell',
+
+
+  propTypes: {
+    children: _react2.default.PropTypes.node,
+    style: _react2.default.PropTypes.object,
+    width: _react2.default.PropTypes.oneOfType([_react2.default.PropTypes.string, _react2.default.PropTypes.number])
+  },
+
+  contextTypes: {
+    muiTheme: _react2.default.PropTypes.object.isRequired
+  },
+
+  render: function render() {
+    var prepareStyles = this.context.muiTheme.prepareStyles;
+
+    var mergedStyles = (0, _simpleAssign2.default)({}, {
+      flex: this.props.width ? null : 1
+    }, this.props.style);
+
+    return _react2.default.createElement(
+      'div',
+      { style: prepareStyles(mergedStyles) },
+      this.props.children
+    );
+  }
+});
+
+var TableList = _react2.default.createClass({
+  displayName: 'TableList',
+
+
+  propTypes: {
+    data: _react2.default.PropTypes.array,
+    headers: _react2.default.PropTypes.array,
+    onHeaderClick: _react2.default.PropTypes.func,
+    onItemClick: _react2.default.PropTypes.func,
+    style: _react2.default.PropTypes.object
+  },
+
+  contextTypes: {
+    muiTheme: _react2.default.PropTypes.object.isRequired
+  },
+
+  getDefaultProps: function getDefaultProps() {
+    return {
+      data: [],
+      headers: []
+    };
+  },
+  _onHeaderClick: function _onHeaderClick(header) {
+    if (this.props.onHeaderClick) {
+      this.props.onHeaderClick(header);
+    }
+  },
+  _onItemClick: function _onItemClick(item) {
+    if (this.props.onItemClick) {
+      this.props.onItemClick(item);
+    }
+  },
+  render: function render() {
+    var _props2 = this.props;
+    var data = _props2.data;
+    var headers = _props2.headers;
+    var style = _props2.style;
+
+    var other = _objectWithoutProperties(_props2, ['data', 'headers', 'style']);
+
+    var prepareStyles = this.context.muiTheme.prepareStyles;
+
+
+    var styles = {
+      root: {},
+      cell: {
+        width: '100%'
+      }
+    };
+    var rowStyle = {
+      display: 'flex',
+      justifyContent: 'space-around',
+      flexFlow: 'row nowrap',
+      alignItems: 'center'
+    };
+
+    var rows = data.map(function (d, i) {
+      var cells = headers.map(function (header, j) {
+        return _react2.default.createElement(
+          TableCell,
+          { width: header.size, key: 'header' + i + j },
+          _react2.default.createElement(
+            'div',
+            { style: header.style },
+            d[header.key]
+          )
+        );
+      }.bind(this));
+
+      return _react2.default.createElement(
+        TableItem,
+        { key: 'data' + i, onTouchTap: this._onItemClick.bind(this, d) },
+        _react2.default.createElement(
+          'div',
+          { style: rowStyle },
+          cells
+        )
+      );
+    }.bind(this));
+
+    // props or theme ?
+    var grey = (0, _colorManipulator.fade)(_colors.darkBlack, 0.4);
+
+    var rowHeaders = headers.map(function (header, i) {
+      var hover = header.text ? _react2.default.createElement(
+        TableItem,
+        {
+          onTouchTap: this._onHeaderClick.bind(this, header),
+          style: { color: grey }
+        },
+        header.text
+      ) : _react2.default.createElement('div', { style: { width: header.size } });
+      return _react2.default.createElement(
+        TableCell,
+        { width: header.size, key: 'header' + i },
+        hover
+      );
+    }.bind(this));
+
+    return _react2.default.createElement(
+      'div',
+      _extends({}, other, { style: prepareStyles(styles.root, style) }),
+      _react2.default.createElement(
+        'div',
+        { style: { paddingLeft: 16, paddingRight: 16 } },
+        _react2.default.createElement(
+          'div',
+          { style: rowStyle },
+          rowHeaders
+        )
+      ),
+      _react2.default.createElement(_Divider2.default, null),
+      rows
+    );
+  }
+});
+
+module.exports = TableList;
